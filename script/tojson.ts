25fb6e1342775676e6587d13d9e7c96bda00636c
see2et <seezet1024@gmail.com>
2023-03-04 20:28:05 +0900

    fix: cannot read csv

--- script/tojson.ts
+++ script/tojson.ts
@@ -1,5 +1,5 @@
 import {parse} from "npm:csv-parse/sync"
 
-const diccsv: string = await Deno.readTextFile("../csv/20230304_F2J.csv");
+const diccsv: string = await Deno.readTextFile("csv/20230304_F2J.csv");
 const dicarray = parse(diccsv, { columns: true })
 console.log(dicarray)
